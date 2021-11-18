62e033dd001d859e426f4c30de9c659ef12cdd8c
Mounir El hadef <moel-had@e1r9p11.1337.ma>
2021-11-18 23:58:00 +0100

    memmove

---
+++ ft_memmove.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moel-had <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/18 21:35:16 by moel-had          #+#    #+#             */
+/*   Updated: 2021/11/18 23:25:46 by moel-had         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, int len)
+{
+	char	*str1;
+	char	*str2;
+	int		i;
+
+	str1 = (char *)dst;
+	str2 = (char *)src;
+	i = 0;
+	if (str1 > str2)
+	{
+		while (len > 0)
+		{
+			str1[len - 1] = str2[len - 1];
+			len--;
+		}
+	}
+	else
+	{
+		while (len > 0)
+		{
+			str1[i] = str2[i];
+			i++;
+			len--;
+		}
+	}
+	return (dst);
+}
