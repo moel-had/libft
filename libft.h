f7c42e48161ca3ac0d0e1e7bdfd19e9c3380a372
Mounir El hadef <moel-had@e1r9p11.1337.ma>
2021-11-18 23:45:49 +0100

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: moel-had <moel-had@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 20:20:28 by moel-had          #+#    #+#             */
-/*   Updated: 2021/11/17 20:24:20 by moel-had         ###   ########.fr       */
+/*   Updated: 2021/11/18 20:21:45 by moel-had         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ char	*ft_substr(char	const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(const char *s1, const char *set);
 char	*ft_itoa(int n);
+size_t	ft_strlen(const char *s);
 
 #endif
