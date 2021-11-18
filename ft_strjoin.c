3928f78180252997dc3c973d51b333c9bd80f1d7
Mounir El hadef <moel-had@e1r9p11.1337.ma>
2021-11-18 21:31:57 +0100

    ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,21 +6,11 @@
 /*   By: moel-had <moel-had@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 13:19:09 by moel-had          #+#    #+#             */
-/*   Updated: 2021/11/16 18:33:31 by moel-had         ###   ########.fr       */
+/*   Updated: 2021/11/18 21:31:09 by moel-had         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int	ft_strlen(char *str)
-{
-	int		i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -32,7 +22,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	len1 = ft_strlen((char *)s1);
 	len2 = ft_strlen((char *)s2);
-	str = malloc(sizeof(char) * (len1 + len2));
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (str == NULL)
 		return (NULL);
 	memmove(str, s1, len1);
