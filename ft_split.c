e95b55b9378652d4acfc35c8be3447c43acce44b
Mounir El hadef <moel-had@e1r2p5.1337.ma>
2021-12-04 00:59:21 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: moel-had <moel-had@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 00:10:50 by moel-had          #+#    #+#             */
-/*   Updated: 2021/11/27 18:47:22 by moel-had         ###   ########.fr       */
+/*   Updated: 2021/12/03 21:47:12 by moel-had         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,8 @@ static char	*ft_dup(const char *src, int start, int finish)
 
 	i = 0;
 	dest = malloc((finish - start + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
 	while (start < finish)
 	{
 		dest[i] = src[start];
@@ -46,16 +48,25 @@ static char	*ft_dup(const char *src, int start, int finish)
 	return (dest);
 }
 
-char	**ft_split(char const *s, char c)
+static void	ft_free(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+static void	ft_write_split(const char *s, char c, char **split)
 {
 	size_t	i;
 	int		j;
 	int		count;
-	char	**split;
 
-	split = malloc((ft_count(s, c) + 1) * sizeof(char *));
-	if (split == NULL)
-		return (NULL);
 	i = 0;
 	j = 0;
 	count = -1;
@@ -66,11 +77,25 @@ char	**ft_split(char const *s, char c)
 		else if ((s[i] == c || i == ft_strlen(s)) && count >= 0)
 		{
 			split[j] = ft_dup(s, count, i);
+			if (split[j] == NULL)
+				ft_free(split);
 			count = -1;
 			j++;
 		}
 		i++;
 	}
 	split[j] = NULL;
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+
+	if (s == NULL)
+		return (NULL);
+	split = malloc((ft_count(s, c) + 1) * sizeof(char *));
+	if (split == NULL)
+		return (NULL);
+	ft_write_split(s, c, split);
 	return (split);
 }
